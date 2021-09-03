3e560cd15c79c40c7859c5faa49018859bc238aa
aiziyuer <ziyu0123456789@gmail.com>
2021-09-03 23:35:59 +0800

    新增配置读取

--- cmd/root.go
+++ cmd/root.go
@@ -1,13 +1,55 @@
 package cmd
 
 import (
+	"bytes"
+	"github.com/gogf/gf/os/gfile"
+	"github.com/gogf/gf/util/gconv"
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"go.uber.org/zap"
+	"gopkg.in/yaml.v3"
+	"io"
+	"os"
 )
+
+type SessionInfo struct {
+	SshUri      string   `yaml:"ssh_uri"`
+	SshIdentity string   `yaml:"ssh_identity"`
+	SshTunnels  []string `yaml:"ssh_tunnels"`
+}
 
 var rootCmd = &cobra.Command{
 	Use: "SshTunnel",
+	RunE: func(cmd *cobra.Command, args []string) error {
+
+		var sessionInfos []SessionInfo
+
+		decoder := yaml.NewDecoder(bytes.NewReader(gfile.GetBytes("sessions.yaml")))
+		for {
+			var s SessionInfo
+			if err := decoder.Decode(&s); err != nil {
+				// Break when there are no more documents to decode
+				if err != io.EOF {
+					return err
+				}
+				break
+			}
+			sessionInfos = append(sessionInfos, s)
+		}
+
+		zap.S().Infof("sessionInfos: %s", gconv.Strings(sessionInfos))
+
+		return nil
+	},
+}
+
+func Execute() {
+
+	if err := rootCmd.Execute(); err != nil {
+		zap.S().Error(err)
+		os.Exit(1)
+	}
+
 }
 
 func init() {
@@ -20,6 +62,6 @@ func initConfig() {
 
 	viper.AutomaticEnv() // read in environment variables that match
 	if err := viper.ReadInConfig(); err == nil {
-		zap.S().Debugf("Using config file:", viper.ConfigFileUsed())
+		zap.S().Debugf("Using config file: %s", viper.ConfigFileUsed())
 	}
 }
--- go.mod
+++ go.mod
@@ -3,11 +3,21 @@ module app
 go 1.15
 
 require (
-	github.com/aiziyuer/connectME v0.0.0-20210718140427-6865414108ac
 	github.com/go-resty/resty/v2 v2.4.0
-	github.com/mitchellh/go-homedir v1.1.0
+	github.com/gogf/gf v1.16.6
+	github.com/magiconair/properties v1.8.4 // indirect
+	github.com/mitchellh/mapstructure v1.4.1 // indirect
+	github.com/pelletier/go-toml v1.8.1 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/spf13/afero v1.5.1 // indirect
+	github.com/spf13/cast v1.3.1 // indirect
 	github.com/spf13/cobra v1.1.3
+	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/viper v1.7.1
+	go.uber.org/multierr v1.6.0 // indirect
 	go.uber.org/zap v1.16.0
+	golang.org/x/tools v0.0.0-20191216052735-49a3e744a425 // indirect
+	gopkg.in/ini.v1 v1.62.0 // indirect
 	gopkg.in/natefinch/lumberjack.v2 v2.0.0
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
 )
