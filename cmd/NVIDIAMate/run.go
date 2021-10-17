d0523e2d0cb7de3ae6fcdac745fc221da9e0ec99
aiziyuer <ziyu0123456789@gmail.com>
2021-10-17 15:49:49 +0800

    新增配置解析

--- cmd/NVIDIAMate/run.go
+++ cmd/NVIDIAMate/run.go
@@ -3,33 +3,62 @@ package main
 import (
 	"app/internal/util"
 
-	"github.com/ghodss/yaml"
-	"github.com/gogf/gf/os/gfile"
+	"github.com/gofrs/uuid"
+
+	"github.com/gogf/gf/util/gconv"
 	"github.com/spf13/cobra"
 	"go.uber.org/zap"
 )
 
-type MigVgpu struct {
-	bus_id string `yaml:"bus_id"`
+// # vf map
+// # |mdev device id                       |name             | mdev_supported_type | gpu profile id |
+// # |b04965e6-a9bb-591f-8f8a-1adcb2c8dc39 |GRID A100-3-20C  | nvidia-476          | 9              |
+type MdeDeviceInfo struct {
+	gpu_bus_id     string
+	mdev_id        string
+	mdev_name      string
+	mdev_type      string
+	gpu_profile_id int
 }
 
 var runCmd = &cobra.Command{
 	Use: "run",
 	RunE: func(cmd *cobra.Command, args []string) error {
 
-		input, err := yaml.YAMLToJSON(gfile.GetBytes("configs/NVIDIAMate.yaml"))
+		configObject, err := util.YamlFileToObject("configs/NVIDIAMate.yaml")
 		if err != nil {
 			return err
 		}
 
-		mig_vgpus, err := util.JsonPathQueryInYamlContent(input, "$.mig_vgpus")
-		if err != nil {
-			return err
+		zap.S().Debugf("configObject: %s.", gconv.String(configObject))
+
+		mdevDevices := make([]*MdeDeviceInfo, 0)
+		gpus := util.MustJsonPathQueryInObject(configObject, "$.mig_vgpus")
+		for gpu_index, gpu := range gpus.([]interface{}) {
+
+			zap.S().Debugf("[gpu-%3d] mig_vgpu: %s", gpu_index, gconv.String(gpu))
+
+			bus_id := util.MustJsonPathQueryInObject(gpu, "$.bus_id")
+			zap.S().Debugf("[gpu-%3d] bus_id: %s", gpu_index, gconv.String(bus_id))
+
+			vgpus := util.MustJsonPathQueryInObject(gpu, "$.partition")
+			zap.S().Debugf("[gpu-%3d] partition: %s", gpu_index, gconv.String(vgpus))
+
+			for vgpu_index, vgpu_name := range vgpus.([]interface{}) {
+
+				mdevDevice := &MdeDeviceInfo{
+					gpu_bus_id: gconv.String(bus_id),
+					mdev_id:    uuid.NewV5(uuid.NamespaceDNS, gconv.String(vgpu_index+1)).String(),
+					mdev_name:  gconv.String(vgpu_name),
+				}
+
+				mdevDevices = append(mdevDevices, mdevDevice)
+
+			}
+
 		}
 
-		for _, mig_vgpu := range mig_vgpus.([]*MigVgpu) {
-			zap.S().Debugf("bus_id: %s", mig_vgpu.bus_id)
-		}
+		zap.S().Infof("mdevDevices: %s", gconv.String(mdevDevices))
 
 		return nil
 	},
