e97e991a455dc26d9482d746d53087416866c880
aiziyuer <ziyu0123456789@gmail.com>
2022-03-01 12:25:01 +0000

    去除特殊的包下载

--- scripts/package.bash
+++ scripts/package.bash
@@ -24,7 +24,7 @@ for APP in $(ls cmd); do
             execute "cp -rf $WORK_DIR/configs ${BUILD_DIR}/"
             execute "export GO111MODULE=on"
             execute "export GOPROXY=https://nexus.moyi-lc.com:5003/repository/go-group/"
-            execute "cd $WORK_DIR && go clean -modcache && go build -v -o ${BUILD_DIR}/${APP} app/cmd/${APP}"
+            execute "cd $WORK_DIR && go build -v -o ${BUILD_DIR}/${APP} app/cmd/${APP}"
             execute "cd ${BUILD_DIR} && tar czvf ../${RELEASE}.tar.gz *"
 
         done
