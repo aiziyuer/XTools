75f23ca5b6f802da0de144a40eb7db33fb0e2893
aiziyuer <ziyu0123456789@gmail.com>
2022-10-19 11:12:19 +0800

    新增server模式

---
+++ cmd/XTunnel/server.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"app/internal/global"
+	"app/internal/xtunnel"
+	"bytes"
+	"fmt"
+	"github.com/gogf/gf/os/gfile"
+	"github.com/mitchellh/go-homedir"
+	"github.com/spf13/cobra"
+	"go.uber.org/zap"
+	"gopkg.in/yaml.v3"
+	"io"
+)
+
+func init() {
+
+	serverCommand := &cobra.Command{
+		Use:   "server",
+		Short: "server",
+		Long:  "server",
+		RunE: func(cmd *cobra.Command, args []string) error {
+
+			var sessionInfos []*xtunnel.SessionInfo
+
+			appName := global.AppConfig().GetAppName()
+			configFilePath, err := homedir.
+				Expand(fmt.Sprintf("~/.config/%s/%s-conf.yaml", appName, appName))
+			if err != nil {
+				zap.S().Fatal(err)
+			}
+
+			decoder := yaml.NewDecoder(bytes.NewReader(gfile.GetBytes(configFilePath)))
+			for {
+				var s xtunnel.SessionInfo
+				if err := decoder.Decode(&s); err != nil {
+					// Break when there are no more documents to decode
+					if err != io.EOF {
+						zap.S().Fatal(err)
+					}
+
+					break
+				}
+
+				// 忽略无效的记录
+				if len(s.SshUri) == 0 || len(s.SshHost) == 0 {
+					continue
+				}
+
+				sessionInfos = append(sessionInfos, &s)
+			}
+
+			h := &xtunnel.TunnelHandler{}
+			_ = h.Do(sessionInfos)
+
+			return nil
+		},
+	}
+
+	rootCmd.AddCommand(serverCommand)
+
+}
